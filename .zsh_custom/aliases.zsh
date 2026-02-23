155ec942d33d8791a3215dee5e306fdcb4dfe9dc
Toshi Kikuchi <toshi@microcooper.com>
2026-02-22 17:54:26 -0800

    update aliases

--- .zsh_custom/aliases.zsh
+++ .zsh_custom/aliases.zsh
@@ -1,8 +1,8 @@
-alias mosh='LC_ALL=en_US.UTF-8 mosh --server="LANG=en_US.UTF-8 mosh-server"'
-
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias grep='grep --color=auto'
+
+alias mail='gnu-mail'
 
 alias rs='eval $(noglob resize)'
 
@@ -11,9 +11,7 @@ alias showpath='echo $PATH | tr : "\n"'
 alias ec='emacs-client'
 alias en='emacs -nw'
 
-alias jd='cd $(jiri -show-root)'
-
-alias ez='exec /bin/zsh'
+alias mosh='LC_ALL=en_US.UTF-8 mosh --server="LANG=en_US.UTF-8 mosh-server"'
 
 # Local Variables:
 # mode: shell-script
